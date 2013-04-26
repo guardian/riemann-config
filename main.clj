c0cadd25253c7d7355257fadd8902ed886147df8
Robert Rees <robert.rees@guardian.co.uk>
2013-04-26 12:34:30 +0100

    Logs the ratio to info and calls alerta if the ratio is above threshold

--- main.clj
+++ main.clj
@@ -109,21 +109,29 @@
 				(splitp < metric
 					0 (add-description  "Puppet resources are failing" warning)
 					(add-description "Puppet is updating all resources" normal)))))
-)
 
-(streams
-	(by [:host]	
-		(moving-time-window 15
+	(streams
+		(by [:host]	
+			(moving-time-window 15
 
-			(smap (fn [events] (
-				(let [success-service-name "gu_200_ok_request_status_rate-frontend"
-					error-service-name "gu_js_diagnostics_rate-frontend"
-					service-filter (fn [service-name event] (= service-name (:service event)))
-					sorted-events (->> events (sort-by :time) reverse)
-					last-success (->> sorted-events (filter (partial service-filter success-service-name)) first)
-					last-error (->> sorted-events (filter (partial service-filter error-service-name)) first)]
-					(if (and last-success last-error)
-						(prn (format "Events seen %d; ratio %f"
-							(count events)
-							(double (/ (:metric last-error) (+ (:metric last-success) (:metric last-error)))))))
-				 prn)))))))
+				(smap (fn [events]
+					(let [success-service-name "gu_200_ok_request_status_rate-frontend"
+						error-service-name "gu_js_diagnostics_rate-frontend"
+						service-filter (fn [service-name event] (and (:metric event) (= service-name (:service event))))
+						sorted-events (->> events (sort-by :time) reverse)
+						last-success (->> sorted-events (filter (partial service-filter success-service-name)) first)
+						last-error (->> sorted-events (filter (partial service-filter error-service-name)) first)
+						threshold 0.1]
+						(if (and last-success last-error)
+							(let [ratio (double (/ (:metric last-error) (+ (:metric last-success) (:metric last-error))))
+									new-event {:host "riemann" :service "frontend_js_error_ratio" :metric ratio}]
+								(do
+									(info
+										(format "Events seen %d; ratio %f; status %s"
+											(count events)
+											ratio
+											(if (> ratio threshold) "bad" "okay")))
+									(if (> ratio threshold)
+										(call-rescue new-event (list critical))
+										(call-rescue new-event (list normal)))))))))))))
+
