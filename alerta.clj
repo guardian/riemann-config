ca3ef935bcd3cd4ef503ebb5268490fcaac6eed7
Nick Satterly <nick.satterly@guardian.co.uk>
2013-06-22 18:28:18 +0100

    revert change to API endpoints used for testing

--- alerta.clj
+++ alerta.clj
@@ -4,8 +4,8 @@
 (def version "1.0.0")
 
 (def alerta-endpoints
-	{:alert "http://localhost:8000/alerta/api/v2/alerts/alert.json"
-	:heartbeat "http://localhost:8000/alerta/api/v2/heartbeats/heartbeat.json"})
+	{:alert "http://monitoring/alerta/api/v2/alerts/alert.json"
+	:heartbeat "http://monitoring/alerta/api/v2/heartbeats/heartbeat.json"})
 
 (defn post-to-alerta
   "POST to the Alerta REST API."
